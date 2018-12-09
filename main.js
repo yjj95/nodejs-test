f28c05a4ee3b9157227e27871870f6c4d9ace28b
jia <15901226963@163.com>
2018-12-09 18:32:16 +0800

    使用promise

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-window.ajax=function({method,url,body,success,fail}){
-    let request=new XMLHttpRequest()
-    request.open(method,url)
-    request.onreadystatechange=function(){
-      if(request.readyState===4){
-          if(request.status>=200&&request.status<=300){
-            //请求成功，调回调
-            let response=JSON.parse(request.responseText)
-            success.call(undefined,response)
-          }else{
-            fail.call(undefined,request.response)
-          }
-      }
-    }
-    request.send(body)
+window.ajax=function({method,url,body}){
+    return new Promise(function(reslove,reject){
+        let request=new XMLHttpRequest()
+        request.open(method,url)
+        request.onreadystatechange=function(){
+            if(request.readyState===4){
+                if(request.status>=200&&request.status<=300){
+                    //请求成功，调回调
+                    let response=JSON.parse(request.responseText)
+                    reslove.call(undefined,response)
+                }else{
+                    reject.call(undefined,request.response)
+                }
+            }
+        }
+        request.send(body)
+    })
+    
   }
 function success(response){
    amount.innerText=response.amount
@@ -25,7 +28,5 @@ button.onclick=()=>{
         'method':'post',
         'url':'/pay',
         'body':'a=1&&b=2',
-        'success':success,
-        'fail':fail
-    })
+    }).then(success,fail)
 }
